3121ab633d09dea7a717ff95c4d06309b6d67484
Jens-Alrik Adrian <jensalrik.adrian@gmail.com>
2022-10-30 21:39:55 +0100

    Data cleaning as node

--- src/amplitude_distribution/pipelines/feature_processing/pipeline.py
+++ src/amplitude_distribution/pipelines/feature_processing/pipeline.py
@@ -5,15 +5,21 @@ generated using Kedro 0.18.3
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import compute_features, create_audio_files
+from .nodes import clean_data, compute_features, create_audio_files
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
+                func=clean_data,
+                inputs="params:data_cleaning_and_creation",
+                outputs=None,
+                name="data_cleaning",
+            ),
+            node(
                 func=create_audio_files,
-                inputs="params:data_creation",
+                inputs="params:data_cleaning_and_creation",
                 outputs=None,
                 name="data_creation",
             ),
