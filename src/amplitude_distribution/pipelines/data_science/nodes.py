a850c214be1cf51e1924cc096b561ab422b339b5
Jens-Alrik Adrian <jensalrik.adrian@gmail.com>
2022-10-30 17:44:16 +0100

    Plot decision boundary

--- src/amplitude_distribution/pipelines/data_science/nodes.py
+++ src/amplitude_distribution/pipelines/data_science/nodes.py
@@ -4,8 +4,11 @@ generated using Kedro 0.18.3
 """
 
 import logging
+
 import pandas
-from sklearn import linear_model, metrics, model_selection
+from kedro.extras.datasets import matplotlib
+from matplotlib import pyplot
+from sklearn import inspection, linear_model, metrics, model_selection
 
 
 def split_data(data: pandas.DataFrame, parameters: dict) -> tuple:
@@ -20,15 +23,41 @@ def split_data(data: pandas.DataFrame, parameters: dict) -> tuple:
 
 
 def train_model(X_train, y_train):
-    classifier = linear_model.LogisticRegression()
+    classifier = linear_model.LogisticRegressionCV()
     classifier.fit(X_train, y_train)
 
     return classifier
 
 
-def test_model(classifier: linear_model.RidgeCV, X_test, y_test):
+def test_model(classifier: linear_model.LogisticRegressionCV, X_test, y_test):
     predictions = classifier.predict(X_test)
     score = metrics.f1_score(y_test, predictions)
 
     logger = logging.getLogger(__name__)
     logger.info("Model has an F1-Score of %.3f on test data.", score)
+
+
+def plot_decision_boundary(
+    classifier: linear_model.LogisticRegressionCV, X_train, y_train, parameters: dict
+):
+    plot_writer = matplotlib.MatplotlibWriter(filepath=parameters["file_path"])
+
+    fig, ax = pyplot.subplots(figsize=(12, 12 / 1.618))
+    inspection.DecisionBoundaryDisplay.from_estimator(
+        classifier,
+        X_train,
+        ax=ax,
+        cmap=pyplot.cm.Paired,
+        response_method="predict",
+        plot_method="pcolormesh",
+    )
+
+    ax.scatter(
+        X_train.iloc[:, 0],
+        X_train.iloc[:, 1],
+        c=y_train,
+        edgecolors="k",
+        cmap=pyplot.cm.Paired,
+    )
+
+    plot_writer.save(fig)
--- src/amplitude_distribution/pipelines/data_science/pipeline.py
+++ src/amplitude_distribution/pipelines/data_science/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.3
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import split_data, train_model, test_model
+from .nodes import plot_decision_boundary, split_data, test_model, train_model
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -21,13 +21,19 @@ def create_pipeline(**kwargs) -> Pipeline:
                 func=train_model,
                 inputs=["X_train", "y_train"],
                 outputs="classifier",
-                name="model_training"
+                name="model_training",
             ),
             node(
                 func=test_model,
                 inputs=["classifier", "X_test", "y_test"],
                 outputs=None,
-                name="model_testing"
-            )
+                name="model_testing",
+            ),
+            node(
+                func=plot_decision_boundary,
+                inputs=["classifier", "X_train", "y_train", "params:plot_options"],
+                outputs=None,
+                name="model_plotting",
+            ),
         ]
     )
